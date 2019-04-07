7ad855503b22b5538898b222a9bcba0c95aa6ea8
Kish Siva <rodes88@msn.com>
2019-04-07 15:23:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def prime?(n)
   
-  2.upto(Math.sqrt(n)) do |x| # testing up to the square root of the number because going past there is excessive
+  2.upto(n) do |x| # testing up to the square root of the number because going past there is excessive
     if n % x == 0
       # n is the number being called from the program
       # x is the number we're dividing by, counting from 2 up to the square root of the number
