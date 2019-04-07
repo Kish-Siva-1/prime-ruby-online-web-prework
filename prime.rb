799cb7cb57fac90779bbf386cd4abd7ae39c78e1
Kish Siva <rodes88@msn.com>
2019-04-07 15:27:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,7 @@ require 'pry'
 def prime?(n)
   
   2.upto(n) do |x| # testing up to the square root of the number because going past there is excessive
+    
     if n % x == 0
       # n is the number being called from the program
       # x is the number we're dividing by, counting from 2 up to the square root of the number
@@ -10,7 +11,7 @@ def prime?(n)
     else
       return true # this means the number is prime
     end 
-    pry.binding
+    
   end
 
 end
