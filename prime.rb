58529766e7b6da7d1b9ad31a6e0cae1fce001dea
Kish Siva <rodes88@msn.com>
2019-04-07 15:08:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,14 +2,20 @@ require 'pry'
 
 def prime?(n)
   
-  counter = 2
+  counter = 1
   
   while counter < n-1
     
-    break if n % counter != 1
+    break if (n % counter != 1 || n % counter != n) 
     counter += 1 
   end
   
+  if n % counter != 1
+    false
+  else
+    true
+  end
+    
 binding.pry
 
 end
